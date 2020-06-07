859fe624fdd81f94cf15ad22452dddbc30141a94
eishin <ec2-user@ip-172-31-24-141.us-east-2.compute.internal>
2020-06-07 08:50:04 +0000

    13課題

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'],function() {
-    Route::get('news/create',
-    'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'],function() {
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 //4
@@ -39,11 +39,11 @@ edit Action に割り当てるように設定してください。
 */
 //admin/profile/create にアクセスしたら ProfileController のadd Action
 //admin/profile/edit にアクセスしたら ProfileController のedit Action
-Route::group(['prefix' => 'admin'],function() {
-    Route::get('profile/create',
-    'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit',
-    'Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'],function() {
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 
 Auth::routes();
