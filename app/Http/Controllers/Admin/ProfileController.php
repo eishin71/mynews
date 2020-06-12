c595271c9e12e01abf7759e079fcaa1342b7cd61
eishin <ec2-user@ip-172-31-24-141.us-east-2.compute.internal>
2020-06-12 05:44:50 +0000

    14課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Profiles;
 class ProfileController extends Controller
 {
     public function add()
@@ -14,6 +12,22 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
+        $this->validate($request,Profiles::$rules);
+        $profiles = new Profiles;
+        $form = $request->all();
+        
+        if(isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profiles->image_path = basename($path);
+        }else {
+            $profiles->image_path = null;
+        }
+        unset($form['_token']);
+        unset($form['image']);
+        
+        $profiles->fill($form);
+        $profiles->save();
+        
         return redirect('admin/profile/create');
     }
     
