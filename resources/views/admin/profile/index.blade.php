6bbe235e434ca9996f67dddac725814824d8e93a
eishin <ec2-user@ip-172-31-24-141.us-east-2.compute.internal>
2020-06-28 10:14:54 +0000

    確認

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -2,7 +2,7 @@
 @section('title','登録済みプロフィール一覧')
 
 @section('content')
-<div class="container">
+    <div class="container">
         <div class="row">
             <h2>プロフィール一覧</h2>
         </div>
@@ -15,7 +15,7 @@
                     <div class="form-group row">
                         <label class="col-md-2">氏名</label>
                         <div class="col-md-8">
-                            <input type="text" class="form-control" name="cond_title" value={{ $cond_title }}>
+                            <input type="text" class="form-control" name="cond_title" value="{{ $cond_title }}">
                         </div>
                         <div class="col-md-2">
                             {{ csrf_field() }}
