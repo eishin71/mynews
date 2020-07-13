90e427fb3a25c7a865a5da4dd2a92ae49834bb91
eishin <ec2-user@ip-172-31-24-141.us-east-2.compute.internal>
2020-07-13 10:32:14 +0000

    heroku編集

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\News;
 use App\History;
 use Carbon\Carbon;
+use Storage;
 
 class NewsController extends Controller
 {
@@ -22,9 +23,9 @@ class NewsController extends Controller
       $form = $request->all();
 
       // formに画像があれば、保存する
-      if ($form['image']) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
+      if (isset($form['image'])) {
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news->image_path = null;
       }
@@ -63,30 +64,32 @@ class NewsController extends Controller
 
 
   public function update(Request $request)
-  {
+    {
         $this->validate($request, News::$rules);
         $news = News::find($request->id);
         $news_form = $request->all();
         if ($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $news_form['image_path'] = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
         }
+
         unset($news_form['_token']);
         unset($news_form['image']);
         unset($news_form['remove']);
         $news->fill($news_form)->save();
-        
+
+        // 以下を追記
         $history = new History;
         $history->news_id = $news->id;
         $history->edited_at = Carbon::now();
         $history->save();
-        
+
         return redirect('admin/news/');
-  }
+    }
   
   public function delete(Request $request)
   {
